c47cffcfa627ffd7b181a2ac3f180dc0b47ff4b7
Chai Tze Chye <hologram38@gmail.com>
2018-10-16 11:28:47 +0800

    Upgrade nuxtjs

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,17 +4,31 @@ module.exports = {
      */
     head: {
         title: 'Reddit style Upvote & Downvote app',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+        meta: [{
+                charset: 'utf-8'
+            },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'Nuxt.js project'
+            }
         ],
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+        link: [{
+            rel: 'icon',
+            type: 'image/x-icon',
+            href: '/favicon.ico'
+        }]
     },
     /*
      ** Customize the progress bar color
      */
-    loading: { color: '#3B8070' },
+    loading: {
+        color: '#3B8070'
+    },
     /*
      ** Build configuration
      */
@@ -22,8 +36,8 @@ module.exports = {
         /*
          ** Run ESLint on save
          */
-        extend(config, { isDev, isClient }) {
-            if (isDev && isClient) {
+        extend(config, { isDev }) {
+            if (isDev && process.client) {
                 config.module.rules.push({
                     enforce: 'pre',
                     test: /\.(js|vue)$/,
